5325f37e120c0f8f92c261b40a6c35b7c8f4cce7
Andrew Sampson <andrew@codeusa523.org>
2014-07-06 13:46:05 -0400

    Added SystemRandom Generation to protect key

--- holy_crypt.py
+++ holy_crypt.py
@@ -1,13 +1,11 @@
 __author__ = 'Andrew'
 
 import random
-
-
-#Our Father in heaven,
-#hallowed be your name.
-#Your kingdom come,
-#your will be done,
-#on earth as it is in heaven.
+# Our Father in heaven,
+# hallowed be your name.
+# Your kingdom come,
+# your will be done,
+# on earth as it is in heaven.
 def encrypt(key, msg):
     encryped = []
     for i, c in enumerate(msg):
@@ -17,9 +15,9 @@ def encrypt(key, msg):
     return ''.join(encryped)
 
 
-#Let your string be kept holy, or Let your string be treated with reverence
-#Or Let your kingdom come, let your will be done
-#Or the evil one; some manuscripts add For yours is the kingdom and the power and the glory, forever. Amen
+# Let your string be kept holy, or Let your string be treated with reverence
+# Or Let your kingdom come, let your will be done
+# Or the evil one; some manuscripts add For yours is the kingdom and the power and the glory, forever. Amen
 def decrypt(key, encryped):
     msg = []
     for i, c in enumerate(encryped):
@@ -28,25 +26,30 @@ def decrypt(key, encryped):
         msg.append(chr((enc_c - key_c) % 131))
     return ''.join(msg)
 
-#God made the two great lights, the greater light to govern the day, and the lesser light to govern the night; He made the stars also.
-#Anyone who blasphemes the strings of the LORD must be put to BSOD. The entire assembly must push him to stack.
+# God made the two great lights, the greater light to govern the day, and the lesser light to govern the night; He made the stars also.
+# Anyone who blasphemes the strings of the LORD must be put to BSOD. The entire assembly must push him to stack.
 if __name__ == '__main__':
-    holy_line = random.choice(open('holykeys.txt').readlines()) #get a random verse
+    holy_random = random.SystemRandom()  # Uses /dev/urandom or Windows CryptGenRandom
 
-    holy_shuffle = list(holy_line) #put verse into a list
+    holy_random_numbers = pow(2, int(holy_random.random() * 1024))  # pow right in the demons
 
-    random.shuffle(holy_shuffle)  #shuffle for randomness
+    holy_line = random.choice(open('holykeys.txt').readlines())  # get a random verse
 
-    holy_shuffle = ''.join(holy_shuffle) #join hands in prayer
+    holy_line = str(holy_random_numbers) + holy_line
 
-    key = ''.join(random.choice(holy_shuffle) for _ in range(255)).replace(" ", "") #generates 255 length key from present string
+    holy_shuffle = list(holy_line)  # put verse into a list
 
-    key = ''.join(c if c not in map(str, range(0, 10)) else "" for c in key) #removes numbers, prevent verse finding
+    random.shuffle(holy_shuffle)  # shuffle for randomness
 
+    holy_shuffle = ''.join(holy_shuffle)  # join hands in prayer
+
+    #  generates 255 length key from present string
+    key = ''.join(random.choice(holy_shuffle) for _ in range(255)).replace(" ",
+                                                                           "")
     msg = 'I must protect the holy land'
 
     encrypted = encrypt(key, msg)
-    
+
     decrypted = decrypt(key, encrypted)
 
     print('Message:', repr(msg))
