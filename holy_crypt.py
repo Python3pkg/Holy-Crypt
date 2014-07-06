3c56df600f26c8a2c881628524d0f1a69d22eb4b
Andrew Sampson <andrew@codeusa523.org>
2014-07-06 15:21:05 -0400

    Preparing for pypi

--- holy_crypt.py
+++ holy_crypt.py
@@ -8,6 +8,7 @@ import string
 def random_key_mask(size=25, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
 
+
 # Uses SystemRandom to try and generate a random number
 # number is then pow'd and multiplied.
 def true_holy_random():
@@ -15,9 +16,11 @@ def true_holy_random():
     holy_random_numbers = pow(2, int(holy_random.random() * 1024))  # pow right in the demons
     return holy_random_numbers
 
+
 # Gets a bible verse from the King James Bible
 def get_verse():
     return random.choice(open('holykeys.txt').readlines())
+
 
 # Our Father in heaven,
 # hallowed be your name.
@@ -25,20 +28,20 @@ def get_verse():
 # your will be done,
 # on earth as it is in heaven.
 def encrypt(key, msg):
-    encryped = []
+    encrypted = []
     for i, c in enumerate(msg):
         key_c = ord(key[i % len(key)])
         msg_c = ord(c)
-        encryped.append(chr((msg_c + key_c) % 131))
-    return ''.join(encryped)
+        encrypted.append(chr((msg_c + key_c) % 131))
+    return ''.join(encrypted)
 
 
 # Let your string be kept holy, or Let your string be treated with reverence
 # Or Let your kingdom come, let your will be done
 # Or the evil one; some manuscripts add For yours is the kingdom and the power and the glory, forever. Amen
-def decrypt(key, encryped):
+def decrypt(key, encrypted):
     msg = []
-    for i, c in enumerate(encryped):
+    for i, c in enumerate(encrypted):
         key_c = ord(key[i % len(key)])
         enc_c = ord(c)
         msg.append(chr((enc_c - key_c) % 131))
@@ -57,23 +60,6 @@ def create_private_key():
     random.shuffle(holy_shuffle)  # shuffle for randomness
 
     holy_shuffle = ''.join(holy_shuffle)  # join hands in prayer
-    key = ''.join(random.choice(holy_shuffle) for _ in range(255)).replace(" ",
+    key = ''.join(random.choice(holy_shuffle) for x in range(255)).replace(" ",
                                                                            "")
     return key
-
-# God made the two great lights, the greater light to govern the day, and the lesser light to govern the night; He made the stars also.
-# Anyone who blasphemes the strings of the LORD must be put to BSOD. The entire assembly must push him to stack.
-if __name__ == '__main__':
-
-    key = create_private_key()
-
-    msg = 'I\'m trying to fight the NSA, work with me on it'
-
-    encrypted = encrypt(key, msg)
-
-    decrypted = decrypt(key, encrypted)
-
-    print('Message:', repr(msg))
-    print('Key:', repr(key))
-    print('Encrypted:', repr(encrypted))
-    print('Decrypted:', repr(decrypted))
