8c06eef820080fdbe3ed91553c13d68832aff38e
Andrew Sampson <andrew@codeusa523.org>
2014-07-06 14:56:57 -0400

    Re-factored some code, added key masking, improved randomness

--- holy_crypt.py
+++ holy_crypt.py
@@ -1,6 +1,24 @@
 __author__ = 'Andrew'
 
 import random
+import string
+
+# Generates 25 random letters and numbers
+# Used to help protect the private key
+def random_key_mask(size=25, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
+
+# Uses SystemRandom to try and generate a random number
+# number is then pow'd and multiplied.
+def true_holy_random():
+    holy_random = random.SystemRandom()  # Uses /dev/urandom or Windows CryptGenRandom
+    holy_random_numbers = pow(2, int(holy_random.random() * 1024))  # pow right in the demons
+    return str(holy_random_numbers)
+
+# Gets a bible verse from the King James Bible
+def get_verse():
+    return random.choice(open('holykeys.txt').readlines())
+
 # Our Father in heaven,
 # hallowed be your name.
 # Your kingdom come,
@@ -26,27 +44,30 @@ def decrypt(key, encryped):
         msg.append(chr((enc_c - key_c) % 131))
     return ''.join(msg)
 
-# God made the two great lights, the greater light to govern the day, and the lesser light to govern the night; He made the stars also.
-# Anyone who blasphemes the strings of the LORD must be put to BSOD. The entire assembly must push him to stack.
-if __name__ == '__main__':
-    holy_random = random.SystemRandom()  # Uses /dev/urandom or Windows CryptGenRandom
 
-    holy_random_numbers = pow(2, int(holy_random.random() * 1024))  # pow right in the demons
+#  generates a private holy key
+# maybe try public/private later
+def create_private_key():
+    holy_line = get_verse()
 
-    holy_line = random.choice(open('holykeys.txt').readlines())  # get a random verse
-
-    holy_line = str(holy_random_numbers) + holy_line
+    holy_line = holy_line + random_key_mask() + true_holy_random()  #With our powers combined
 
     holy_shuffle = list(holy_line)  # put verse into a list
 
     random.shuffle(holy_shuffle)  # shuffle for randomness
 
     holy_shuffle = ''.join(holy_shuffle)  # join hands in prayer
-
-    #  generates 255 length key from present string
     key = ''.join(random.choice(holy_shuffle) for _ in range(255)).replace(" ",
                                                                            "")
-    msg = 'I must protect the holy land'
+    return key
+
+# God made the two great lights, the greater light to govern the day, and the lesser light to govern the night; He made the stars also.
+# Anyone who blasphemes the strings of the LORD must be put to BSOD. The entire assembly must push him to stack.
+if __name__ == '__main__':
+
+    key = create_private_key()
+
+    msg = 'I\'m trying to fight the NSA, work with me on it'
 
     encrypted = encrypt(key, msg)
 
